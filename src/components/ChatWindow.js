73c4fc62b16f1264728fead4925433acc044ed65
Quan Gan <quan@ztag.com>
2023-03-14 20:11:11 -0700

    recording and download audio file

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import MicRecorder from 'mic-recorder-to-mp3';
 import './ChatWindow.css';
+
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
 const ChatWindow = () => {
   const [messages, setMessages] = useState([]);
+  const [isRecording, setIsRecording] = useState(false);
 
   const handleSendMessage = (message) => {
     setMessages([...messages, { text: message, sender: 'user' }]);
@@ -11,6 +15,36 @@ const ChatWindow = () => {
       ...prevMessages,
       { text: 'Bot response', sender: 'bot' },
     ]);
+  };
+
+  const startRecording = async () => {
+    try {
+      await Mp3Recorder.start();
+      setIsRecording(true);
+    } catch (error) {
+      console.error('Failed to start recording:', error);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      const [buffer, blob] = await Mp3Recorder.stop().getMp3();
+      setIsRecording(false);
+
+      const fileName = `tempRecordings/recording_${Date.now()}.mp3`;
+      const file = new File(buffer, fileName, {
+        type: blob.type,
+        lastModified: Date.now(),
+      });
+
+      // Save the file to local drive (only works on localhost or local development environment)
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(file);
+      a.download = fileName;
+      a.click();
+    } catch (error) {
+      console.error('Failed to stop recording:', error);
+    }
   };
 
   return (
@@ -40,6 +74,12 @@ const ChatWindow = () => {
             }
           }}
         />
+        <button
+          className={`btn btn-${isRecording ? 'danger' : 'primary'}`}
+          onClick={isRecording ? stopRecording : startRecording}
+        >
+          {isRecording ? 'Stop Recording' : 'Start Recording'}
+        </button>
       </div>
     </div>
   );
